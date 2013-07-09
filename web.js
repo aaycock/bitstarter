d1a18c5894579687f24ade4c17dfe4d45540e8fb
Ubuntu <ubuntu@ip-172-31-47-51.us-west-2.compute.internal>
2013-07-09 05:59:01 +0000

    modified web.js to read from fs

--- web.js
+++ web.js
@@ -3,7 +3,14 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ // response.send('Hello World 2!');
+  var fs = require('fs');  
+  fs.readFile('./index.html', function (err, data) {
+  if (err) throw err;
+  //console.log(data);
+    var buffer = new Buffer(data);
+    response.send(buffer.toString('utf8',0,buffer.length));
+});
 });
 
 var port = process.env.PORT || 5000;
